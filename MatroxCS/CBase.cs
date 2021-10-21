2ea3cb0908ae5fd4fa591ca11b31fedb638abdc2
悟 比良 <S150231@sintokogio.jp>
2021-10-21 17:53:22 +0900

    Cameraクラスのカメラオープン処理追加

--- MatroxCS/CBase.cs
+++ MatroxCS/CBase.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Matrox.MatroxImagingLibrary;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace MatroxCS
 {
--- MatroxCS/Camera.cs
+++ MatroxCS/Camera.cs
@@ -6,37 +6,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Matrox.MatroxImagingLibrary;
+using System.Runtime.InteropServices;
 
 namespace MatroxCS
 {
     class CCamera : CBase
     {
         #region メンバー変数
-        //  デジタイザID
-        private MIL_ID m_milDigitizer = MIL.M_NULL;
-        //  カメラ映像を画面に表示するときの画像バッファ
-        private MIL_ID m_milShowImage = MIL.M_NULL;
-        //  グラブ専用リングバッファ 2固定
-        private MIL_ID[] m_milGrabImageArray = { MIL.M_NULL, MIL.M_NULL };
 
-        //  画像サイズ。カメラ画像バッファもカメラ映像用バッファも同サイズ
-        private Size m_szImageSize;
-        //  カメラインスタンスID
-        private int m_iCameraID;
+        private MIL_ID m_milDigitizer = MIL.M_NULL;                                 //  デジタイザID
+        private MIL_ID m_milShowImage = MIL.M_NULL;                          //  カメラ映像を画面に表示するときの画像バッファ
+        private MIL_ID[] m_milGrabImageArray = { MIL.M_NULL, MIL.M_NULL };   //  グラブ専用リングバッファ 2固定
+        private MIL_DIG_HOOK_FUNCTION_PTR m_delProcessingFunctionPtr;               //  
+        private GCHandle m_handUserData_doThrough;                                  //  
+        private GCHandle m_handUserData_ProcessingFunction;                         //  
+        private CCamera m_cCamera;
+        private Size m_szImageSize;                                                 //  画像サイズ。カメラ画像バッファもカメラ映像用バッファも同サイズ
+        private int m_iCameraID;                                                    //  カメラインスタンスID
+        private string m_strCameraFilePath;                                         //	DCFファイル名
+        private string m_strIPAddress;                                              //  カメラのIPアドレス
 
         //  差分撮ったり、カメラ画像をリアルタイムで評価しないといけないならこのクラスでやるしかない
         //  必要に応じて画像バッファとかアルゴリズムを追加する。
         //  ただ不要なときは画像バッファを確保したりしないようにしたい
 
-
-
         //  以下各カメラパラメーター
-
-        //  ゲイン
-        private double m_dGain;
-        //  露光時間(単位：μs)
-        private long m_lShtterSpeed;
+        private double m_dGain;                                             //  ゲイン
+        private long m_lShtterSpeed;                                        //  露光時間(単位：μs)
         //  他いろいろ
+        private bool m_bThroughFlg = false;
 
         #endregion
 
@@ -57,15 +55,32 @@ namespace MatroxCS
             //  カメラメーカーとか、画像サイズとか、インターフェースとか、ゲインとか、IPアドレスとか
 
             //  デジタイザオープン
+            if (m_iBoardType != (int)MTX_TYPE.MTX_HOST)
+            {
+                //	デジタイザID取得
+                if (m_strIPAddress != "")
+                {
+                    MIL.MdigAlloc(m_smilSystem, MIL.M_GC_CAMERA_ID(m_strIPAddress), m_strCameraFilePath, MIL.M_GC_DEVICE_IP_ADDRESS, ref m_milDigitizer);
+                }
+                else
+                {
+                    MIL.MdigAlloc(m_smilSystem, MIL.M_DEV0, m_strCameraFilePath, MIL.M_DEFAULT, ref m_milDigitizer);
+                }
 
+                if (m_milDigitizer == MIL.M_NULL)
+                {
+                    return -1;
+                }
+            }
             //  グラブ専用バッファ確保
-
+            MIL.MbufAllocColor(m_smilSystem, 3, m_szImageSize.Width, m_szImageSize.Height, 8 + MIL.M_UNSIGNED, MIL.M_IMAGE + MIL.M_PROC + MIL.M_PACKED + MIL.M_BGR24, ref m_milGrabImageArray[0]);
+            MIL.MbufAllocColor(m_smilSystem, 3, m_szImageSize.Width, m_szImageSize.Height, 8 + MIL.M_UNSIGNED, MIL.M_IMAGE + MIL.M_PROC + MIL.M_PACKED + MIL.M_BGR24, ref m_milGrabImageArray[1]);
             //  カメラが無事オープン出来たらIDを割り当てる
             m_iCameraID = m_siNextCameraID;
             //  次のカメラで被らないようにインクリメントしておく
             m_siNextCameraID++;
             //  IDが最大値まで行ったらリセットする
-            if(m_siNextCameraID >= m_siCameraOffsetID + m_siIDMaxLength)
+            if (m_siNextCameraID >= m_siCameraOffsetID + m_siIDMaxLength)
             {
                 m_siNextCameraID = m_siCameraOffsetID;
             }
