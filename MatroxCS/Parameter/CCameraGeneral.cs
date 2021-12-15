7151c7e7041b998c94c205c37e4c5f6b5b15b57b
悟 比良 <S150231@sintokogio.jp>
2021-12-15 14:20:15 +0900

    MatroxCS/Parameterフォルダー追加

---
+++ MatroxCS/Parameter/CCameraGeneral.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatroxCS.Parameter
+{
+    class CCameraGeneral
+    {
+        public int Number { get; set; }         // カメラ個数
+        public int BoardType { get; set; }      // ボード種類 
+        public int HeartBeatTime { get; set; } // ハートビート時間(単位:s)
+        public List<CCameraInfo> CameraInformation { get; private set; } = new List<CCameraInfo>(); // カメラの詳細情報
+    }
+}
---
+++ MatroxCS/Parameter/CCameraInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatroxCS.Parameter
+{
+    class CCameraInfo
+    {
+        public string IdentifyName { get; set; }    // 識別ネーム
+        public int CameraType { get; set; }         // 現在未使用
+        public string CameraFile { get; set; }      // DCFファイルパス
+        public int Width { get; set; }              // 取得画像幅
+        public int Height { get; set; }             // 取得画像高さ
+        public int Color { get; set; }              // 現在未使用
+        public int ImagePose { get; set; }          // 現在未使用
+        public int UseSerialComm { get; set; }      // 現在未使用
+        public int COMNo { get; set; }              // 現在未使用
+        public string IPAddress { get; set; }       // gigeカメラのIPアドレス
+    }
+}
